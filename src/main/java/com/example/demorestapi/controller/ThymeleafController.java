cb1dd165cabecd0a6c9298bd9477d7838a536978
StefanD <stefan.durla@gmail.com>
2023-12-10 15:34:30 +0200

    Example of views loading or redirect

--- src/main/java/com/example/demorestapi/controller/ThymeleafController.java
+++ src/main/java/com/example/demorestapi/controller/ThymeleafController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/ui")
@@ -12,5 +14,18 @@ public class ThymeleafController {
     public String helloWorld(Model model){
        model.addAttribute("text", "Ana are mere, dar vrea pere!");
         return "helloWorld";
+    }
+
+    @GetMapping("1")
+    public ModelAndView helloWorldModelAndView(){
+        ModelAndView response = new ModelAndView();
+        response.addObject("text", "Ana are mere, dar vrea pere!");
+        response.setViewName("helloWorld");
+        return response;
+    }
+
+    @GetMapping("2")
+    public RedirectView helloWorldRedirect(){
+        return new RedirectView("/ui");
     }
 }
