5cc8188385dd526806f28627a2a56565b83afe20
StefanD <stefan.durla@gmail.com>
2023-12-16 10:05:21 +0200

    Added iteration in thymeleaf

--- src/main/java/com/example/demorestapi/controller/ThymeleafController.java
+++ src/main/java/com/example/demorestapi/controller/ThymeleafController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/ui")
 public class ThymeleafController {
@@ -40,6 +42,8 @@ public class ThymeleafController {
            response.setViewName("helloWorld");
        }else {
            response.addObject("object", form);
+           response.addObject("listOfPeople", List.of("Ana", "Maria", "Elena","Stefan","Darius"));
+           response.addObject("listOfAges", List.of(39, 45, 30));
            response.setViewName("displayValues");
        }
         return response;
